42d1ed18d3fcba7762d18a93d8ed007f5a0136fa
Denis Woldemariam <damewold@gmail.com>
2020-03-28 15:20:27 -0500

    created database and table using sql

--- data.sql
+++ data.sql
@@ -1,6 +1,15 @@
-CREATE TABLE "koalas"(
-    "id" PRIMARY KEY,
-    "status" VARCHAR(60) NOT NULL,
-    "tasks" VARCHAR(250) NOT NULL,
-    "dueDate" SMALLINT NOT NULL,
+CREATE TABLE "tasksTable"(
+    "id" SERIAL PRIMARY KEY,
+    "task" VARCHAR(250) NOT NULL,
+    "dueDate" DATE
 );
+
+INSERT INTO "tasksTable" ("task","dueDate") VALUES('Finish the weekend challenge', '03/29/2020');
+INSERT INTO "tasksTable" ("task","dueDate") VALUES('Do your laundry', '03/28/2020');
+INSERT INTO "tasksTable" ("task","dueDate") VALUES('Study and read some books', '03/28/2020');
+INSERT INTO "tasksTable" ("task","dueDate") VALUES('Buy some Groceries', '03/29/2020');
+INSERT INTO "tasksTable" ("task","dueDate") VALUES('Practice meditation and self-reflection', '03/29/2020');
+INSERT INTO "tasksTable" ("task","dueDate") VALUES('Write some poems', '03/28/2020');
+INSERT INTO "tasksTable" ("task","dueDate") VALUES('Clean the kitchen', '03/28/2020');
+INSERT INTO "tasksTable" ("task","dueDate") VALUES('Get your room in order', '03/28/2020');
+INSERT INTO "tasksTable" ("task","dueDate") VALUES('Cook some food', '03/28/2020');
